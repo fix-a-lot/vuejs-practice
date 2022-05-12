611cb0c8133099677cac2954fe4a4cf924512d5f
fixalot <fixalot@anyssign.com>
2022-05-12 22:07:05 +0900

    Update

--- public/examples/vue/TemplateLiteral.js
+++ public/examples/vue/TemplateLiteral.js
@@ -1,11 +1,18 @@
 export const TemplateLiteral = {
   template: `
     <p>응애 나 아기 컴포넌트</p>
+    <div><button type="button" @click="probe">probe</button></div>
     <p>{{ message }}</p>
   `,
   data() {
     return {
       message: 'This is template literal message'
+    }
+  },
+  methods: {
+    probe() {
+      debugger;
+      console.log('probe');
     }
   }
 };
