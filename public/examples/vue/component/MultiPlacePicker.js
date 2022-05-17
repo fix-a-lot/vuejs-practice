87112aabe5d760ed2c1aa2ed4cbcaba9438b0037
fixalot <fixalot@anyssign.com>
2022-05-17 21:27:25 +0900

    Update

---
+++ public/examples/vue/component/MultiPlacePicker.js
@@ -0,0 +1,20 @@
+export const MultiPlacePicker = {
+  template: `
+    <select id="multiPlacePicker" placeholder="체크인장소 선택">
+      <option value="">체크인장소 선택</option>
+      <option value="A1234">전체</option>
+      <option value="A2345">전략기획본부실</option>
+      <option value="A3456">영업본부실</option>
+      <option value="B4567">국내영업팀</option>
+      <option value="B5678">우리집</option>
+      <option value="B6789">너네집</option>
+    </select>
+  `,
+  mounted() {
+    new TomSelect('#multiPlacePicker', {
+      plugins: ['checkbox_options'],
+      maxItems: 99,
+      hideSelected: false,
+    });
+  }
+};
---
+++ public/examples/vue/component/SingleUpperOrgPicker.js
@@ -0,0 +1,33 @@
+export const SingleUpperOrgPicker = {
+  template: `
+    <select id="singleUpperOrgPicker" placeholder="상위조직 없음">
+      <option value="">상위조직 없음</option>
+      <option data-depth="0" value="A1234">A 조직</option>
+      <option data-depth="1" value="A2345">A-1 조직</option>
+      <option data-depth="2" value="A3456">A-1-Z 조직</option>
+      <option data-depth="0" value="B4567">B 조직</option>
+      <option data-depth="1" value="B5678">B-1 조직</option>
+      <option data-depth="1" value="B6789">B-2 조직</option>
+    </select>
+  `,
+  mounted() {
+    new TomSelect('#singleUpperOrgPicker', {
+      closeAfterSelect: true,
+      maxItems: 1,
+      hideSelected: false,
+      render: {
+        option: function(data, escape) {
+          console.log(data.depth);
+          let blanks = '';
+          for (let i = 0; i < data.depth; i++) {
+            blanks += '&nbsp;&nbsp;&nbsp;&nbsp;';
+          }
+          return '<div>' +
+              '<span>' + blanks + '</span>' +
+              '<span class="url">' + escape(data.text) + '</span>' +
+            '</div>';
+        }
+      }
+    });
+  }
+};
