34e568f632f8adddd69fe60b97538d03374f02b3
fixalot84 <fixalot@anyssign.com>
2022-05-26 15:24:18 +0900

    Update

--- public/lib/air-datepicker/locale-kr.js
+++ public/lib/air-datepicker/locale-kr.js
@@ -5,7 +5,7 @@ const localeKr = {
   daysShort: ['일', '월', '화', '수', '목', '금', '토'],
   daysMin: ['일', '월', '화', '수', '목', '금', '토'],
   months: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
   today: 'Today',
   clear: 'Clear',
   dateFormat: 'yyyy-MM-dd',
