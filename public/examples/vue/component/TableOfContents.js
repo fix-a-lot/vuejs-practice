985cb23709ac31d3ebe42d2252f6246eb70ec712
fixalot <fixalot@anyssign.com>
2022-06-02 23:55:14 +0900

    Update

--- public/examples/vue/component/TableOfContents.js
+++ public/examples/vue/component/TableOfContents.js
@@ -3,7 +3,7 @@ export const TableOfContents = {
     <template v-if="headers">
       <h4>📌 목차</h4>
       <ul>
-        <li v-for="item in headers">
+        <li v-for="item in headers" :style="{ 'margin-left': item.marginLeft }">
           <a :href="item.href">{{item.title}}</a>
         </li>
       </ul>
@@ -16,14 +16,16 @@ export const TableOfContents = {
     };
   },
   mounted() {
-    let nodeList = document.querySelectorAll('h2');
+    let nodeList = document.querySelectorAll('h2, h3');
     let headers = [];
     nodeList.forEach((ele) => {
       let newId = ele.innerText.replace(/\s/g, '-');
       headers.push({
         title: ele.innerText,
-        href: '#' + newId
+        href: '#' + newId,
+        marginLeft: ele.tagName == 'H2' ? 'auto' : '15px'
       });
+      // 사실 마진 넣는건 꼼수고 딴 애덜은 nested ul로 처리했던데 어케한겨 😵‍💫
       ele.setAttribute('id', newId);
       this.headers = headers;
     });
